7d2b8c313b8be7f90ee397dd77aa8400c1d0d600
John Law <johnlaw.po@gmail.com>
2021-12-02 23:11:20 +0800

    improve util

--- src/util/src/input.rs
+++ src/util/src/input.rs
@@ -3,7 +3,7 @@ use std::io::BufRead;
 use std::io::BufReader;
 use std::io::ErrorKind;
 
-pub fn get_input_integer(id: &str) -> Vec<i64> {
+fn get_reader(id: &str) -> BufReader<File> {
     // read from file at "src/<id>/src/input.txt"
     let root_loc = "src/".to_owned() + id + "/src/input.txt";
     let lib_loc = "src/input.txt";
@@ -18,8 +18,11 @@ pub fn get_input_integer(id: &str) -> Vec<i64> {
             other_error => panic!("Problem opening the file: {:?}", other_error),
         },
     };
-    let reader = BufReader::new(file);
-    let numbers: Vec<i64> = reader
+    BufReader::new(file)
+}
+
+pub fn get_input_integer(id: &str) -> Vec<i64> {
+    let numbers: Vec<i64> = get_reader(id)
         .lines()
         .map(|line| line.unwrap().parse::<i64>().unwrap())
         .collect();
@@ -27,22 +30,7 @@ pub fn get_input_integer(id: &str) -> Vec<i64> {
 }
 
 pub fn get_input_string(id: &str) -> Vec<String> {
-    // read from file at "src/<id>/src/input.txt"
-    let root_loc = "src/".to_owned() + id + "/src/input.txt";
-    let lib_loc = "src/input.txt";
-    let file = File::open(root_loc);
-    let file = match file {
-        Ok(file) => file,
-        Err(error) => match error.kind() {
-            ErrorKind::NotFound => match File::open(lib_loc) {
-                Ok(fc) => fc,
-                Err(e) => panic!("Problem opening the file: {:?}", e),
-            },
-            other_error => panic!("Problem opening the file: {:?}", other_error),
-        },
-    };
-    let reader = BufReader::new(file);
-    let strings: Vec<String> = reader
+    let strings: Vec<String> = get_reader(id)
         .lines()
         .map(|l| l.expect("Could not parse string from file"))
         .collect();
