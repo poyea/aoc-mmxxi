e681fa728db7b792abb04496fd5033f6eddca01b
John Law <johnlaw.po@gmail.com>
2021-12-06 17:24:19 +0800

    improve template

--- src/00/src/main.rs
+++ src/00/src/main.rs
@@ -1,7 +1,12 @@
 use util::dummy::new_year;
 
 #[allow(dead_code)]
-pub fn solve() -> i64 {
+pub fn solve_part1() -> i64 {
+    new_year()
+}
+
+#[allow(dead_code)]
+pub fn solve_part2() -> i64 {
     new_year()
 }
 
@@ -10,11 +15,17 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_solve() {
-        assert_eq!(solve(), 2022);
+    fn test_solve_part1() {
+        assert_eq!(solve_part1(), 2022);
+    }
+
+    #[test]
+    fn test_solve_part2() {
+        assert_eq!(solve_part1(), 2022);
     }
 }
 
 fn main() {
-    println!("{}", solve());
+    println!("{}", solve_part1());
+    println!("{}", solve_part2());
 }
